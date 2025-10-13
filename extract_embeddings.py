b214a10b7423f5065c3cfbc77298c119118c6c23
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2025-10-13 16:15:16 +0200

    .

--- extract_embeddings.py
+++ extract_embeddings.py
@@ -2,14 +2,14 @@ import pandas as pd
 import numpy as np
 import torchaudio
 import torch
-from transformers import AutoModel, AutoProcessor
+from transformers import AutoModel, Wav2Vec2FeatureExtractor
 from tqdm import tqdm
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 model_name = "m-a-p/MERT-v1-330M"
-model = AutoModel.from_pretrained(model_name, trust_remote_code=True).to(device).eval()
-processor = AutoProcessor.from_pretrained(model_name, trust_remote_code=True, use_fast=False)
+model = AutoModel.from_pretrained(model_name, torch_dtype=torch.bfloat16, trust_remote_code=True).to(device).eval()
+processor = Wav2Vec2FeatureExtractor.from_pretrained(model_name, trust_remote_code=True, use_fast=False)
 
 df = pd.read_csv("labels.csv")
 embs, labels = [], []
@@ -17,14 +17,19 @@ embs, labels = [], []
 for _, row in tqdm(df.iterrows(), total=len(df)):
     path = row.filepath
     waveform, sr = torchaudio.load(path)
-    if sr != 24000:  # MERT expects 24 kHz
-        waveform = torchaudio.functional.resample(waveform, sr, 24000)
-    inputs = processor(audios=waveform.squeeze(0), sampling_rate=24000, return_tensors="pt").to(device)
-    with torch.no_grad():
-        out = model(**inputs)
     
-    # Take mean of last hidden layer embeddings
-    vec = out.last_hidden_state.mean(dim=1).cpu().numpy().squeeze()
+    resample_rate = processor.sampling_rate
+    if sr != resample_rate:
+        resampler = torchaudio.transforms.Resample(sr, resample_rate)
+        waveform = resampler(waveform)
+    
+    input_audio = waveform.squeeze(0).numpy()
+    inputs = processor(input_audio, sampling_rate=resample_rate, return_tensors="pt").to(device)
+    
+    with torch.no_grad():
+        outputs = model(**inputs)
+    
+    vec = outputs.last_hidden_state.mean(dim=1).cpu().numpy().squeeze()
     embs.append(vec)
     labels.append(row.label)
 
