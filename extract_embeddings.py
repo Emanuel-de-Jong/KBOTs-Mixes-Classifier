69bb87d663df7c3d52ecc3db486c6e1e883f5923
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2025-10-13 17:42:36 +0200

    embeddings finally!!!

--- extract_embeddings.py
+++ extract_embeddings.py
@@ -5,7 +5,7 @@ import torch
 from transformers import AutoModel, Wav2Vec2FeatureExtractor
 from tqdm import tqdm
 
-CHUNK_LENGTH_SECONDS = 2.0
+CHUNK_LENGTH_SECONDS = 10.0
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -16,12 +16,8 @@ processor = Wav2Vec2FeatureExtractor.from_pretrained(model_name, trust_remote_co
 df = pd.read_csv("labels.csv")
 embs, labels = [], []
 
-i = 0
+song_batch_count = 0
 for _, row in tqdm(df.iterrows(), total=len(df)):
-    i += 1
-    if i > 5:
-        break
-    
     path = row.filepath
     waveform, sr = torchaudio.load(path)
 
@@ -30,7 +26,11 @@ for _, row in tqdm(df.iterrows(), total=len(df)):
         resampler = torchaudio.transforms.Resample(orig_freq=sr, new_freq=resample_rate).cpu()
         waveform = resampler(waveform.cpu())
 
-    audio_samples = waveform.squeeze(0)
+    if waveform.dim() > 1:
+        audio_samples = waveform.mean(dim=0)
+    else:
+        audio_samples = waveform.squeeze(0)
+    
     samples_per_chunk = int(CHUNK_LENGTH_SECONDS * resample_rate)
     
     total_samples = len(audio_samples)
@@ -54,18 +54,16 @@ for _, row in tqdm(df.iterrows(), total=len(df)):
         
         chunk_vec = outputs.last_hidden_state.mean(dim=1).cpu().float().numpy().squeeze()
         chunk_embeddings.append(chunk_vec)
-        
-        del inputs, outputs
-        if device == "cuda":
-            torch.cuda.empty_cache()
-    
-    del waveform, audio_samples
     
     if chunk_embeddings:
         file_vec = np.mean(chunk_embeddings, axis=0) 
         embs.append(file_vec)
         labels.append(row.label)
         print(f"Successfully processed {len(chunk_embeddings)} chunks")
+
+        song_batch_count += 1
+        # if song_batch_count > 25:
+        #     break
 
 X = np.stack(embs)
 pd.Series(labels).to_csv("y_labels.csv", index=False)
