3a3693c2b58970474ae5a4f811bdfb9eb3c83187
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2025-10-13 16:33:19 +0200

    .

--- extract_embeddings.py
+++ extract_embeddings.py
@@ -19,22 +19,19 @@ embs, labels = [], []
 for _, row in tqdm(df.iterrows(), total=len(df)):
     path = row.filepath
     waveform, sr = torchaudio.load(path)
-    
+
     resample_rate = processor.sampling_rate
     if sr != resample_rate:
-        resampler = torchaudio.transforms.Resample(sr, resample_rate)
-        waveform = resampler(waveform)
+        resampler = torchaudio.transforms.Resample(orig_freq=sr, new_freq=resample_rate).cpu()
+        waveform = resampler(waveform.cpu())
 
     audio_samples = waveform.squeeze(0)
     samples_per_chunk = int(CHUNK_LENGTH_SECONDS * resample_rate)
     
     chunks = torch.split(audio_samples, samples_per_chunk)
-    
     chunk_embeddings = []
-    
     for chunk in chunks:
-        input_audio_chunk = chunk.numpy() 
-        
+        input_audio_chunk = chunk.numpy()
         inputs = processor(
             input_audio_chunk, 
             sampling_rate=resample_rate, 
@@ -46,6 +43,12 @@ for _, row in tqdm(df.iterrows(), total=len(df)):
         
         chunk_vec = outputs.last_hidden_state.mean(dim=1).cpu().numpy().squeeze()
         chunk_embeddings.append(chunk_vec)
+        
+        del inputs, outputs
+        if device == "cuda":
+            torch.cuda.empty_cache() 
+    
+    del waveform, audio_samples, chunks
     
     file_vec = np.mean(chunk_embeddings, axis=0) 
     
