cd9f6d92498b8630e2bf7c8af1bc362f78dad98d
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2026-02-20 20:23:13 +0100

    .

--- s4_infer/comments.js
+++ s4_infer/comments.js
@@ -19,5 +19,104 @@ let comments = {
         'Club': {
             'alts': ['Acid_Trance',],
         },
+        'Deep_House': {
+            'alts': ['Ambient_Techno',],
+        },
+        'DnB': {
+            'alts': ['Jazzstep',],
+        },
+        'Hardwave': {
+            'alts': ['Melodic_Dubstep',],
+        },
+        'Liquid_DnB': {
+            'alts': ['DnB',],
+        },
+        'Mainstream_Liquid_DnB': {
+            'alts': ['Liquid_DnB',],
+        },
+        'Progressive_House': {
+            'alts': ['Groovy_House',],
+        },
+        'Psytrance': {
+            'alts': ['Goa_Trance',],
+        },
+        'Liquid_DnB': {
+            'alts': ['DnB',],
+        },
+    },
+    'edm_balance': {
+        'Acid_Techno': {
+            'alts': ['Hard_Techno',],
+        },
+        'Chill_Slap_House': {
+            'alts': ['Synth_Funk',],
+        },
+        'Deep_House': {
+            'alts': ['Progressive_House',],
+        },
+        'Liquid_DnB': {
+            'alts': ['Jazzstep',],
+        },
+        'Progressive_House': {
+            'alts': ['Techno',],
+        },
+        'Psytrance': {
+            'alts': ['Goa_Trance',],
+        },
+        'Techno': {
+            'alts': ['Hard_Techno',],
+        },
+    },
+    'edm_outliers': {
+        'Chill_Slap_House': {
+            'alts': ['Deep_House',],
+        },
+        'Club': {
+            'alts': ['Breakbeat',],
+        },
+        'Deep_House': {
+            'alts': ['Ambient_Techno',],
+        },
+        'Hardwave': {
+            'alts': ['Melodic_Dubstep',],
+        },
+        'Liquid_DnB': {
+            'alts': ['DnB',],
+        },
+        'Mainstream_Liquid_DnB': {
+            'alts': ['Liquid_DnB',],
+        },
+        'Progressive_House': {
+            'alts': ['Techno',],
+        },
+        'Psytrance': {
+            'alts': ['Goa_Trance',],
+        },
+        'Techno': {
+            'alts': ['Hard_Techno',],
+        },
+    },
+    'edm_vol_norm': {
+        'Club': {
+            'alts': ['Acid_Trance',],
+        },
+        'Deep_House': {
+            'alts': ['Ambient_Techno',],
+        },
+        'DnB': {
+            'alts': ['Jazzstep',],
+        },
+        'Liquid_DnB': {
+            'alts': ['DnB',],
+        },
+        'Mainstream_Liquid_DnB': {
+            'alts': ['Liquid_DnB',],
+        },
+        'Progressive_House': {
+            'alts': ['Techno',],
+        },
+        'Psytrance': {
+            'alts': ['Goa_Trance',],
+        },
     },
 };
